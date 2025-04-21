a635515880baef527d76004fb68f17bd42da09cb
Lakshay Goel <130988683+Lakshayg001@users.noreply.github.com>
2025-04-22 00:11:52 +0530

    Update basic.tsx

--- components/welcome/demo/basic.tsx
+++ components/welcome/demo/basic.tsx
@@ -5,7 +5,7 @@ const Demo = () => {
   return (
     <Welcome
       icon="https://mdn.alipayobjects.com/huamei_iwk9zp/afts/img/A*s5sNRo5LjfQAAAAAAAAAAAAADgCCAQ/fmt.webp"
-      title="Hello, I'm Ant Design X"
+      title="Hello Sir, I'm Ant Design X"
       description="Base on Ant Design, AGI product interface solution, create a better intelligent vision~"
     />
   );
